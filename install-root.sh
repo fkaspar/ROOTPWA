8273360bc67b401536b9f660c84477a4714b42fa
fkaspar <florian.kaspar@tum.de>
2015-04-26 10:52:09 +0200

    geändert:       .travis.yml 	geändert:       install-root.sh

--- install-root.sh
+++ install-root.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
-set -ex
+#!/bin/bash
+set -exv
 wget http://root.cern.ch/download/root_v5.34.30.source.tar.gz
 tar -xzf root_v5.34.30.source.tar.gz
 cd root && ./configure --enable-mathmore --enable-minuit2 && make 
-. bin/thisroot.sh
+source bin/thisroot.csh
