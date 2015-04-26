f9b084b6dc579903345f3f32ef9c6101c4f48eeb
fkaspar <florian.kaspar@tum.de>
2015-04-26 14:34:14 +0200

    geändert:       .travis.yml 	geändert:       install-libconfig.sh

--- install-libconfig.sh
+++ install-libconfig.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -exv
-mkdir ./libconfig && cd ./libconfig
 wget http://www.hyperrealm.com/libconfig/libconfig-1.4.9.tar.gz
 tar -xzf libconfig-1.4.9.tar.gz
 mkdir libconfig_install
-./libconfig-1.4.9/configure --prefix=$PWD/libconfig_install
+export PARENT_DIR=$PWD
+cd ./libconfig-1.4.9
+./configure --prefix=$PARENT_DIR/libconfig_install
 make && make install
