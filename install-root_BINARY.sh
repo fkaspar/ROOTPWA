76260ef72b272143c6475b56ee1eb1e276b3718e
Florian Kaspar <florian.kaspar@tum.de>
2015-05-03 14:20:49 +0200

    geändert:       install-root_BINARY.sh

--- install-root_BINARY.sh
+++ install-root_BINARY.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -exv
 echo $PATH
-wget https://docs.google.com/uc?export=download&confirm=gQWw&id=0Bxmj9rGnkOZqZzRPUzZ2VEFQMk0 #ftp://root.cern.ch/root/root_v5.32.04.Linux-slc5_amd64-gcc4.3.tar.gz
+wget https://www.dropbox.com/s/vbpvign4f4sr13k/root.tar.gz
 tar -xzf root.tar.gz
