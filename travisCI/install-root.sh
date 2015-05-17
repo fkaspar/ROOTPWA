ebd4f27bff13a727fd5324827a9e9879ab014f3e
fkaspar <florian.kaspar@tum.de>
2015-05-17 17:48:10 +0200

    geändert:       ../.travis.yml 	geändert:       install-root.sh

--- travisCI/install-root.sh
+++ travisCI/install-root.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -exv
 echo $PATH
-if [ $ROOT_VER=5.34.30 ]; then wget https://www.dropbox.com/s/vbpvign4f4sr13k/root.tar.gz; fi 
-if [ $ROOT_VER=6.02.08 ]; then wget https://www.dropbox.com/s/kzihg34enf2niqt/root-6.02.08.tar.gz; fi
+if [ "$ROOT_VER" = "5.34.30" ]; then wget hhttps://www.dropbox.com/s/hfy0e4zch7oug0k/root-5.34.30.tar.gz; fi 
+if [ "$ROOT_VER" = "6.02.08" ]; then wget https://www.dropbox.com/s/kzihg34enf2niqt/root-6.02.08.tar.gz; fi
 tar -xzf root-$ROOT_VER.tar.gz
