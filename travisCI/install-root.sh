1236ae2905dd2fc6f240a4ea10ed4f80eb1558e4
fkaspar <florian.kaspar@tum.de>
2015-05-17 19:10:53 +0200

    geändert:       travisCI/install-root.sh

--- travisCI/install-root.sh
+++ travisCI/install-root.sh
@@ -2,6 +2,6 @@
 set -exv
 echo $PATH
 mkdir root-$ROOT_VER
-if [ "$ROOT_VER" = "5.34.30" ]; then wget https://www.dropbox.com/s/hfy0e4zch7oug0k/root-5.34.30.tar.gz -C ./root-$ROOT_VER --strip-components=1; fi 
-if [ "$ROOT_VER" = "6.02.08" ]; then wget https://www.dropbox.com/s/kzihg34enf2niqt/root-6.02.08.tar.gz -C ./root-$ROOT_VER --strip-components=1; fi
-tar -xzf root-$ROOT_VER.tar.gz
+if [ "$ROOT_VER" = "5.34.30" ]; then wget https://www.dropbox.com/s/hfy0e4zch7oug0k/root-5.34.30.tar.gz; fi 
+if [ "$ROOT_VER" = "6.02.08" ]; then wget https://www.dropbox.com/s/kzihg34enf2niqt/root-6.02.08.tar.gz; fi
+tar -xzf root-$ROOT_VER.tar.gz -C ./root-$ROOT_VER --strip-components=1
