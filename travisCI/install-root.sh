92dbb3de13d1507b627ff502d1eb3cff8df17d8e
fkaspar <florian.kaspar@tum.de>
2015-05-17 15:47:01 +0200

    geändert:       ../.travis.yml 	geändert:       install-root.sh 	reconfigured travis to build root-pwa with root6

--- travisCI/install-root.sh
+++ travisCI/install-root.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -exv
 echo $PATH
-wget https://www.dropbox.com/s/vbpvign4f4sr13k/root.tar.gz
-tar -xzf root.tar.gz
+#wget https://www.dropbox.com/s/vbpvign4f4sr13k/root.tar.gz
+wget https://www.dropbox.com/s/kzihg34enf2niqt/root-6.02.08.tar.gz
+tar -xzf root-6.02.08.tar.gz
