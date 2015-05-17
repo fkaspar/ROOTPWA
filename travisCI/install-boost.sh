351f4ce27f0e3d384522551c2b0f213fa657abd7
fkaspar <florian.kaspar@tum.de>
2015-05-17 18:20:45 +0200

    geändert:       ../.travis.yml 	geändert:       install-boost.sh 	geändert:       install-root.sh

--- travisCI/install-boost.sh
+++ travisCI/install-boost.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -exv
-wget 'http://downloads.sourceforge.net/project/boost/boost/1.58.0/boost_1_58_0.tar.gz'
-tar -xzf boost_1_58_0.tar.gz
+#wget 'http://downloads.sourceforge.net/project/boost/boost/1.58.0/boost_1_58_0.tar.gz'
+#tar -xzf boost_1_58_0.tar.gz
+
+wget 'https://www.dropbox.com/s/wf8ds7qdesarr1s/boost_1_58_0_bin.tar.gz'
+tar -xzf boost_1_58_0_bin.tar.gz
