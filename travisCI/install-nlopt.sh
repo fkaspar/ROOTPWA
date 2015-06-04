84e45fcdfb07c7f7772ec84a9244a2345bc9dd00
fkaspar <florian.kaspar@tum.de>
2015-06-04 19:19:16 +0200

    modified:   travisCI/install-nlopt.sh 		added '--with-cxx' so C++ algorithms are compiled

--- travisCI/install-nlopt.sh
+++ travisCI/install-nlopt.sh
@@ -6,4 +6,4 @@ mkdir -p ${PARENT_DIR}/nlopt-install
 wget http://ab-initio.mit.edu/nlopt/nlopt-2.4.2.tar.gz
 tar -xzf nlopt-2.4.2.tar.gz -C ${PARENT_DIR}/nlopt-build --strip-components=1
 cd ${PARENT_DIR}/nlopt-build
-./configure --prefix=${PARENT_DIR}/nlopt-install && make && make install
+./configure --prefix=${PARENT_DIR}/nlopt-install --with-cxx && make && make install
