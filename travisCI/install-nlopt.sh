686fa5b3f903cd4d9aec41c419c94c09a97f1c7e
fkaspar <florian.kaspar@tum.de>
2015-06-04 18:23:56 +0200

    modified:   .travis.yml 		updated to install nlopt correctly 	modified:   travisCI/install-nlopt.sh

--- travisCI/install-nlopt.sh
+++ travisCI/install-nlopt.sh
@@ -2,7 +2,8 @@
 set -exv
 export PARENT_DIR=${PWD}
 mkdir -p ${PARENT_DIR}/nlopt-build
+mkdir -p ${PARENT_DIR}/nlopt-install
 wget http://ab-initio.mit.edu/nlopt/nlopt-2.4.2.tar.gz
 tar -xzf nlopt-2.4.2.tar.gz -C ${PARENT_DIR}/nlopt-build --strip-components=1
 cd ${PARENT_DIR}/nlopt-build
-./configure && make && sudo make install
+./configure --prefix=${PARENT_DIR}/nlopt-install && make && make install
